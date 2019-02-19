143d658af6bcf08830302d22e2d7c52d11efbfae
HanlinMiao <46973263+HanlinMiao@users.noreply.github.com>
2019-02-19 19:25:30 +0000

    Update BigNum.hh

--- hw3bignumber_addition/BigNum.hh
+++ hw3bignumber_addition/BigNum.hh
@@ -5,14 +5,14 @@
 class BigNum {
 private:
 	// 1234
-	uint32_t a, b, c, d; // a is the high order part
+	uint32_t *a; // a is the high order part
 public:
 	BigNum() {}
-	BigNum(uint32_t v) : a(0), b(0), c(0), d(v) {}
+	BigNum(uint32_t *input) : a(input){}
 	
 	friend BigNum add128(BigNum& dest, const BigNum& left, const BigNum& right); // dest = left + right
 
 	friend std::ostream& operator <<(std::ostream& s, const BigNum& big) {
-		return s << big.a << "," << big.b << "," << big.c << "," << big.d;
+		return s << big.a[0] << "," << big.a[1]<< "," << big.a[2] << "," << big.a[3];
 	}
 };
